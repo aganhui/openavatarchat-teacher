db08399f23734b23c08e89d491664108f5cefba2
aganhui <907402005@qq.com>
2025-09-21 12:28:21 +0800

    fix ssl cert

--- scripts/create_ssl_certs.sh
+++ scripts/create_ssl_certs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-openssl req -newkey rsa:2048 -nodes -keyout ./ssl_certs/localhost.key -x509 -days 365 -out ./ssl_certs/localhost.crt
+openssl req -newkey rsa:2048 -nodes -keyout ./ssl_certs/localhost.key -x509 -days 365 -out ./ssl_certs/localhost.crt -subj "/C=US/ST=State/L=City/O=Company/CN=localhost"
